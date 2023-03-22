ef285a55aa83c919493c47fc00a7e9ab60a9d4f8
bilal khan <bilalb2cinfosolutions@gmail.com>
2023-03-22 13:15:30 +0530

    object

---
+++ object-questions.md
@@ -0,0 +1,121 @@
+## object
+
+#### 1. What is an object in JavaScript?
+
+``` object ek datastructure hain. jiska use data store krane ke liye kiya jata hain.
+           data store key or value ki form me huti he
+```
+#### 2.What is the difference between dot notation and bracket notation for accessing object properties?
+
+```dot notation:- esme hum value . likhe ke nikalte for example : obj.keyname
+bracket notation:-esme hum valu [] se nikalte he for example : obj.['key'name]
+```
+ #### 3 How do you loop through the properties of an object in JavaScript ?
+```let obj = {
+    f_name: "bilal",
+    l_name: "khan",
+    mobile: 234234254,
+    emai: "bilal@gmail.com",
+};
+type 1
+let val = Object.entries(obj);
+for (let i = 0; i < val.length; i++) {
+    console.log(val[i][0] ," ", val[i][1]);
+}
+type 2
+for (const key in obj) {
+  console.log(`${key}  ${obj[key]}`);
+}
+```
+
+#### 4.What is the difference between an object and an array in JavaScript?
+```array:-
+       array ek datastructure jiska use data store kervane ke kiye kiya jata hain.emse value ko index se nikalte he.
+object:-
+       object bhi  ek datastructure jiska use data store kervane ke kiye kiya jata hain.emse hum value key pairs se nikalte he
+```
+
+####  5 Write a JavaScript function to convert an object into a list of [key, value] pairs.
+```
+let obj ={
+    f_name: "bilal",
+    l_name: "khan",
+    mobile: 234234254,
+    emai: "bilal@gmail.com",
+   address:{
+        picode:3232,
+        addhar:323245,
+        place:"karkwal",
+    }
+}
+console.log(Object.entries(obj));
+console.log(Object.keys(obj));
+console.log(Object.values(obj));
+
+function pairs(objs) {
+    let newObj =[];
+    for (const key in objs) {
+        newObj.push([key, objs[key]])
+    }
+    return newObj;
+}
+console.log(pairs(obj));
+```
+
+#### 6 Write a function that takes an object representing a person and returns their full name.
+
+```
+let obj ={
+    named:"bilal",
+    l_name:"khan",
+}
+function person(newObj) {
+    let {named ,l_name} = newObj
+     return `${named}  ${l_name}`;
+}
+console.log(person(obj));
+```
+
+#### 7 Create an Object with your personal details. Now print all the keys of the object in ascending order.
+
+```
+let obj ={
+    f_name: "bilal",
+    l_name: "khan",
+    mobile: 234234254,
+    emai: "bilal@gmail.com",
+   address:{
+        picode:3232,
+        addhar:323245,
+        place:"karkwal",
+    }
+};
+let ans = Object.entries(obj);
+console.log(ans.sort());
+for (const value of ans) {
+    console.log(value[0], value[1]);
+}
+document.write(ans);
+```
+
+#### 8 . Create an Object with your personal details. Now filter out all the values of the object and show them in descending order.
+```
+let personalDetails = {
+  name: "bilal",
+  mobile: 6375164100,
+  fatherName: "usman khan",
+  village: "karkwal",
+};
+let key = Object.values(personalDetails);
+let ans = key.sort(sorting);
+console.log(ans);
+function sorting(val1, val2) {
+  return val2.length - val1.length;
+}
+```
+
+#### 9 . Create an object to hold information on your favorite recipe. It should have properties for title (a string), servings (a number), and ingredients (an array of strings).On separate lines (one console.log statement for each), log the recipe information so it looks like:   Mole Serves: 2 Ingredients: cinnamon cumin cocoa
+
+#### 10 . Create a JavaScript function inside an object which finds max of 3 numbers. Now call this function of the object and print the maximum number.
+
+
--- test4.js
+++ test4.js
@@ -1,152 +1,154 @@
-// Question 1
-// Write a JavaScript program to check whether a string "Code" presents at 5th (index 4) position in a given string, if "Code"
-//  presents in the string return the string without "Code" otherwise return the original one.
-
-// function stringF(str)
-// {
-//   if (str.length < 4) {
-//     return str;
-//   }
-//   let ans_str = str;
-//   if (str.substring(10, 6) == 'Code') 
-//     {
-//    ans_str = str.substring(0, 4)
-//   }
-//   return ans_str;
+// 3 How do you loop through the properties of an object in JavaScript ?
+// let obj = {
+//     f_name: "bilal",
+//     l_name: "khan",
+//     mobile: 234234254,
+//     emai: "bilal@gmail.com",
+// };
+// type 1
+// let val = Object.entries(obj);
+// for (let i = 0; i < val.length; i++) {
+//     console.log(val[i][0] ," ", val[i][1]);
 // }
-// console.log(stringF("WeacadCode"));
-
-// Question 2
-// Write a JavaScript program to capitalize the first letter of each word of a given string.
-
-// function capiStr(str) 
-// {
-//     str = str.split(" ");
-//     for (let i = 0; i < str.length; i++) {
-//       str[i] = str[i][0].toUpperCase() + str[i].substr(1);
-//   }
-//     return str.join(" ");
+// type 2
+// for (const key in obj) {
+//   console.log(`${key}  ${obj[key]}`);
 // }
 
-// console.log(capiStr("the quick brown fox"));
+// 5 Write a JavaScript function to convert an object into a list of [key, value] pairs.
 
-
-// Question 3
-// Write a JavaScript program to check whether all the digits in a given number are the same or not.
-
-// function same(num) {
-//   var first = num % 10;
-//   while (num) {
-//     if (num % 10 !== first) return "number is not some";
-//        num = Math.floor(num / 10);
-//   }
-//   return "number is some"
-// }
-// console.log(same(1234));
-// console.log(same(222));
-
-
-// Question 4
-// Write a JavaScript function that reverse a number.
-
-// let number = 123456789;
-// function reverse(num)
-// {
-// 	 num = num +"";
-// 	return  num.split("").reverse().join("");
-// }
-// console.log(Number(reverse(number)));
-
-
-// Question 5
-// Write a JavaScript function to extract unique characters from a string.
-
-// let string = "thequickbrownfoxjumpsoverthelazydog"
-// function UniqueStr(str){
-//   let unique = "";
-//   for(let i = 0; i < str.length; i++){
-//     if(!unique.includes(str[i])){
-//       unique += str[i]
+// let obj ={
+//     f_name: "bilal",
+//     l_name: "khan",
+//     mobile: 234234254,
+//     emai: "bilal@gmail.com",
+//    address:{
+//         picode:3232,
+//         addhar:323245,
+//         place:"karkwal",
 //     }
-//   }
-//   return unique;
 // }
+// console.log(Object.entries(obj));
+// console.log(Object.keys(obj));
+// console.log(Object.values(obj));
 
-// console.log(UniqueStr(string));
-
-// Question 6
-// Write a JavaScript function to chop a string into chunks of a given length. Test Data :
-
-// function Chop(str){
-//      let chunk = [];
-// for (let i = 0; i < str.length; i += 3) {
-//     chunk.push(str.substring(i, i + 3));
+// function pairs(objs) {
+//     let newObj =[];
+//     for (const key in objs) {
+//         newObj.push([key, objs[key]])
+//     }
+//     return newObj;
 // }
-// return chunk;
+// console.log(pairs(obj));
+
+// 6 Write a function that takes an object representing a person and returns their full name.
+
+// let obj ={
+//     named:"bilal",
+//     l_name:"khan",
 // }
-// console.log(Chop('w3resource'));
-// console.log(Chop('w3resource',2));
-// console.log(Chop('w3resource',3));
-
-// Question 7
-// Write a JavaScript function to find a word within a string. Test Data :
-
-// function search_word(text, word){
-    
-//   var x = 0, y=0;
- 
-//   for (i=0;i< text.length;i++)
-//       {
-//       if(text[i] == word[0])
-//           {
-//           for(j=i;j< i+word.length;j++)
-//              {
-//               if(text[j]==word[j-i])
-//                 {
-//                   y++;
-//                 }
-//               if (y==word.length){
-//                   x++;
-//               }
-//           }
-//           y=0;
-//       }
-//   }
-//  return "'"+word+"' was found "+x+" times.";
+// function person(newObj) {
+//     let {named ,l_name} = newObj
+//      return `${named}  ${l_name}`;
 // }
-// console.log(search_word('The quick brown fox', 'fox'));
-// console.log(search_word('aa, bb, cc, dd, aa', 'aa'));
+// console.log(person(obj));
+
+// 7 Create an Object with your personal details. Now print all the keys of the object in ascending order.
+
+// let obj ={
+//     f_name: "bilal",
+//     l_name: "khan",
+//     mobile: 234234254,
+//     emai: "bilal@gmail.com",
+//    address:{
+//         picode:3232,
+//         addhar:323245,
+//         place:"karkwal",
+//     }
+// };
+// let ans = Object.entries(obj);
+// console.log(ans.sort());
+// for (const value of ans) {
+//     console.log(value[0], value[1]);
+// }
+// document.write(ans);
 
 
-// Question 8
-// Create an array of numbers. Now filter out all the numbers from the array where number is in between 30-50. After filtering the numbers, add 20 to each number and finally print the sum of all numbers using reduce function.
-
-// Question 9
-// Convert below array[[1,2], [3,4], [5,6], [7,8], [9,10]] to [3,7,11,15,19]
-
-let a = [[1,2], [3,4], [5,6], [7,8], [9,10]];
-let newArr = [];
-for (let i = 0; i < a.length; i++) {
-  let sum = a[i][0] + a[i][1];
-  newArr.push(sum);
+// 8 . Create an Object with your personal details. Now filter out all the values of the object and show them in descending order.
+let personalDetails = {
+  name: "bilal",
+  mobile: 6375164100,
+  fatherName: "usman khan",
+  village: "karkwal",
+};
+let key = Object.values(personalDetails);
+let ans = key.sort(sorting);
+console.log(ans);
+function sorting(val1, val2) {
+  return val2.length - val1.length;
 }
-console.log(newArr);
 
-// Question 10
-// Print below pattern
+// 9 . Create an object to hold information on your favorite recipe. It should have properties for title (a string), servings (a number), and ingredients (an array of strings).
+// On separate lines (one console.log statement for each), log the recipe information so it looks like:
+// Mole
+// Serves: 2
+// Ingredients:
+// cinnamon
+// cumin
+// cocoa
 
-// let n = 5;
-// let string = "";
-// for (let i = 1; i <= n; i++) {
-//   for (let j = 1; j < i; j++) {
-//     string += " ";
-//   }
-//   for (let k = 1; k <= 2 * (n - i + 1) - 1; k++) {
-//     string += k;
-//   }
-//   string += "\n";
+// 10 . Create a JavaScript function inside an object which finds max of 3 numbers. Now call this function of the object and print the maximum number.
+
+
+///////////////////////////////////maping//////////////////////
+
+
+// let map = new Map();
+//  map.set("name","bilal");
+//  map.set("mobile",234234 );
+
+//  console.log(map.size);
+//  console.log(map.get("name"));
+//  console.log(map.has("name"));
+//   console.log(map.has("name"));
+// for (const value of map) {
+//     console.log(value[0] , value[1]);
 // }
-// console.log(string);
+//  map.forEach(test);
+//  function test(value ,key) {
+//     console.log(key ,value);
+//  }
+
+/////////////////map to arrya//////////////////////////
+// let map = new Map();
+// map.set("name","bilal");
+// map.set("number",2434234);
+// map.set("picode",444);
+// console.log([...map]);
+
+// let obj = {
+//     name: "Wecode",
+//     mobile: 54654656,
+//     address: {
+//       pincode: 302012,
+//       city: "Jaipur",
+//     },
+//   };
+//   //Value Update
+//   obj.name ="bilal",
+//   obj.mobile = 11111111111;
+//   obj["mobile"] = 2222222222;
+//   obj.address.pincode = 3333333;
+//    delete obj.address.pincode, 
+//   console.log(obj);
+
+
+
+
+
+
+  
+
 
 
 
