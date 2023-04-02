663b28a723a10e46b3443739ec824c70aee098d0
bilal khan <bilalb2cinfosolutions@gmail.com>
2023-04-02 14:03:49 +0530

    dd

---
+++ excries.js
@@ -0,0 +1,166 @@
+// exesrise - 4
+// Question 1 Take two numbers and take a value which should be the sum of the two numbers plus some other number. Now findout the 3rd number.
+// let a = 50;
+// let b = 20;
+// let sum = 100;
+// console.log(sum -(a + b));
+
+// Question 2
+// Ask the below questions:
+// What is your English marks in 10th?
+// What is your Science marks in 10th?
+// What is your Math marks in 10th?
+// What is your Hindi marks in 10th?
+// What is your SST marks in 10th? Now calculate percentage by taking all the 5 subjects numbers.
+
+// let English = Number(prompt("What is your English marks in 10th?"));
+// let Science = Number(prompt("What is your Science marks in 10th?"));
+// let Math = Number(prompt("What is your Math marks in 10th?"));
+// let Hindi = Number(prompt("What is your Hindi marks in 10th?"));
+// let SST = Number(prompt("What is your SST marks in 10th?"));
+
+// let total = English + Science + Math + Hindi + SST ;
+// let marks = 500;
+// let subtotal = total / marks * 100
+// console.log(subtotal);
+
+// Question 3
+// In which century were your born? find out by asking birth year. Use Math.floor function to remove decimal portion of the century.
+// let birthYear = Number(prompt('In which century were your born?'));
+
+// let ans = Math.floor(birthYear/100)+1
+// console.log(ans);
+
+// Question 4
+// Add 20 in your name and check value in console.
+// Add true to your name and check output
+// Add 30+"learnjavascript" and check type of the final value.
+
+// console.log("learnjavascript" + 20);
+// console.log(true + "learnjavascript");
+// console.log(30 + "learnjavascript");
+
+// Question 5
+// Create Reverse Game. Ask any question with 2 possible answers. If user selects first option then show second option in alert and vice versa.
+// For example: Do you drink coffee or tea? and If user says coffee then show Tea in alert and if user says tea then show coffee.
+  
+//  let  drinks = prompt("Do you drink coffee or tea?");    
+//  if (drinks === "coffee") {
+//     console.log("tea");
+//  }
+//  else if(drinks === "tea") {
+//     console.log("coffee");
+//  }
+//  else{
+//     console.log("wrong choice");
+//  }
+
+// Question 6
+// Test below conditions using &&, || and not logical operators. 
+// Take any number check if number is even and also check if number is multiple of 4 or not.
+//  If both conditions are true then check if number is less than 100 or number is 84 or not.
+
+// let num = 20;
+// if (num % 2 ===0 && num % 4 === 0 && (num < 100 || num === 84) ) {
+//     console.log("yes");
+// }
+// else{
+//     console.log("no");
+// }
+
+
+// Question 7 
+//   Take city temprature.
+//   If temp is less than 10 degree then print very cold.
+//   If city temp is less than 30 and greater than equal to 10 then print cold.
+//   If temp is between 30 to 40 then print Hot. Else print Very hot
+
+// let temp = 44;
+// if (temp < 10) {
+//     console.log('very cold');
+// }
+// else if (temp <  30 && temp >=10){
+// console.log('cold');
+// }
+// else if(temp <= 40 && temp >=  30 ){
+//   console.log("Hot");
+// }
+// else{
+//     console.log("very hot");
+// }
+
+
+
+
+// Question 8
+// Using prompt ask user about his/her age.
+//  If user is greater than 18 then show user
+//   message "Do you want to open account?" If yes click
+//    on Okay button then ask your 4 below questions
+// What is your name ?
+// What is your aaadhar number ?
+// What is your address?
+// What is your mobile number?
+//  And print all these details in console. 
+//  If click on cancel then show message "Sorry, please visit again."
+
+// let age = Number(prompt("what is your age"));
+// if (age > 18 ) {
+//   let ok = confirm("Do you want to open account?");
+//   if (ok) {
+//      let name = prompt("What is your name ?");
+//      let aaadharNumber = Number(prompt("What is your aaadhar Number ?"));
+//      let address = prompt("What is your address?");
+//      let mobileNumber = Number(prompt("What is your mobile number?"));
+//      console.log(name ,aaadharNumber,address,mobileNumber)
+//   }
+// }
+// else{
+//     confirm("Sorry, please visit again.");
+// }
+
+
+// Question 9
+// Create a simple calcultor. Firslty ask your about the operation.
+//  Like "What do you want to do?" User can type choice in any way
+//  like +,- or plus, minus or add, sub etc. After that ask user
+//  about first number and then second number and finally show the 
+//  desired output.
+
+// let ans = prompt("What do you want to peform?");
+// let num1 = Number(prompt("Enter number 1"));
+// let num2 = Number(prompt("Enter number 2"));
+// if (ans === "+") {
+//     console.log(num1 + num2);
+// }
+// else if( ans === "-"){
+//       console.log(num1 - num2);
+// }
+// else if ( ans === "*"){
+// console.log(num1 * num2);
+// }
+// else {
+//     console.log("please try again");
+// }
+
+// Question 10
+// Who is most intelligent in your family? 
+// Ask 10th percentage of each family member and check who got the highest marks.
+// If total family members are more than 4 then you can use Math.max() function
+ 
+// let member1 = Number(prompt("what is bilal 10th percentage ?"));
+// let members2 = Number(prompt("what is altaf 10th percentage ?"));
+// let members3 = Number(prompt("what is A L 10th percentage ?"));
+// let members4 = Number(prompt("what is sher mohammad 10th percentage ?"));
+
+// let sumMark = Math.max(member1 , members2 , members3 , members4);
+// console.log(sumMark);
+
+
+
+
+
+    
+
+
+
--- test4.js
+++ test4.js
@@ -1,104 +1,3 @@
-// 3 How do you loop through the properties of an object in JavaScript ?
-// let obj = {
-//     f_name: "bilal",
-//     l_name: "khan",
-//     mobile: 234234254,
-//     emai: "bilal@gmail.com",
-// };
-// type 1
-// let val = Object.entries(obj);
-// for (let i = 0; i < val.length; i++) {
-//     console.log(val[i][0] ," ", val[i][1]);
-// }
-// type 2
-// for (const key in obj) {
-//   console.log(`${key}  ${obj[key]}`);
-// }
-
-// 5 Write a JavaScript function to convert an object into a list of [key, value] pairs.
-
-// let obj ={
-//     f_name: "bilal",
-//     l_name: "khan",
-//     mobile: 234234254,
-//     emai: "bilal@gmail.com",
-//    address:{
-//         picode:3232,
-//         addhar:323245,
-//         place:"karkwal",
-//     }
-// }
-// console.log(Object.entries(obj));
-// console.log(Object.keys(obj));
-// console.log(Object.values(obj));
-
-// function pairs(objs) {
-//     let newObj =[];
-//     for (const key in objs) {
-//         newObj.push([key, objs[key]])
-//     }
-//     return newObj;
-// }
-// console.log(pairs(obj));
-
-// 6 Write a function that takes an object representing a person and returns their full name.
-
-// let obj ={
-//     named:"bilal",
-//     l_name:"khan",
-// }
-// function person(newObj) {
-//     let {named ,l_name} = newObj
-//      return `${named}  ${l_name}`;
-// }
-// console.log(person(obj));
-
-// 7 Create an Object with your personal details. Now print all the keys of the object in ascending order.
-
-// let obj ={
-//     f_name: "bilal",
-//     l_name: "khan",
-//     mobile: 234234254,
-//     emai: "bilal@gmail.com",
-//    address:{
-//         picode:3232,
-//         addhar:323245,
-//         place:"karkwal",
-//     }
-// };
-// let ans = Object.entries(obj);
-// console.log(ans.sort());
-// for (const value of ans) {
-//     console.log(value[0], value[1]);
-// }
-// document.write(ans);
-
-
-// 8 . Create an Object with your personal details. Now filter out all the values of the object and show them in descending order.
-let personalDetails = {
-  name: "bilal",
-  mobile: 6375164100,
-  fatherName: "usman khan",
-  village: "karkwal",
-};
-let key = Object.values(personalDetails);
-let ans = key.sort(sorting);
-console.log(ans);
-function sorting(val1, val2) {
-  return val2.length - val1.length;
-}
-
-// 9 . Create an object to hold information on your favorite recipe. It should have properties for title (a string), servings (a number), and ingredients (an array of strings).
-// On separate lines (one console.log statement for each), log the recipe information so it looks like:
-// Mole
-// Serves: 2
-// Ingredients:
-// cinnamon
-// cumin
-// cocoa
-
-// 10 . Create a JavaScript function inside an object which finds max of 3 numbers. Now call this function of the object and print the maximum number.
-
 
 ///////////////////////////////////maping//////////////////////
 
@@ -144,11 +43,476 @@ function sorting(val1, val2) {
 
 
 
+// let map = new Map();
+// map.set("name","bilal");
+// map.set("email","bilal@gmail.com");
+// map.set("password",455345);
+// console.log([...map]);
+
+// let arr =[ ["js", 500],
+// ["designing", 400],];
+// let map =  new Map(arr);
+// console.log(map);
+
+// let obj = {
+//   name:"bilal",
+//   email:"bilal@gmail.com",
+//   password:423545,
+// }
+// let objjjj = new Map(Object.entries(obj));
+// console.log(objjjj);
 
 
-
+// let arr = [1, 2, 1, 2, 3, 2, 5];
+// let map = new Map();
+// for (let key of arr) {
+//   map.set(key, 1);
+// }
+// console.log(map);
   
 
+// let arr = [
+//     1, 2, 2, 2, 2, 1, 2, 3, 4, 5, 6, 1, 1, 7, 8, 9, 10, 11, 2, 1, 4, 6, 7,
+//   ];
 
+//   for (const value of arr) {
+//     console.log(value);
+//   }
+
+// let arr = [1,2,3,4,5,6]
+
+// console.log(arr.filter((num) => num % 2 === 1).map(num=> num+1).reduce((num1,num2) =>  num2 + num1));
+  
+// function declerationFun(a , b) {
+//     return a + b; 
+// }
+// console.log(declerationFun(20 ,30));
+//  const expreceFun = function(a , b) {
+//     console.log(a + b);
+//  }
+//  expreceFun(20 ,40);
+
+//  const add = function (a, b) {
+//     //function expression
+//     console.log(a + b);
+//   };
+//   add(10, 20);
+
+
+// function decFun(a ,b) {
+//     return a + b;
+// }
+// console.log(decFun(20,40));
+
+// const expreceFun = function(a ,b){
+//     return a +b;
+// }
+// console.log(expreceFun(10,20));
+
+// const arrow = (a ,b) =>{
+//      return a + b;
+// }
+// console.log(arrow(22,23));
+
+// sum(10, 20);
+// function sum(a, b) {
+//   //function declaration
+//   console.log(a + b);
+// }
+// const add = function (a, b) {
+//   //function expression
+//   console.log(a + b);
+// };
+// add(10, 20);
+// const plus = (a, b) => {
+//   //arrow function
+//   console.log(a + b);
+// };
+// plus(10, 20);
+
+// 11
+// 14
+// 0
+// function decFun(num) {
+//     if (num % 2===1) {
+//         return "odd number";
+//     }
+//     else{
+//         return "Even Number";
+//     }
+// }
+// let ans = decFun(11);
+// console.log(ans);
+
+//  const expFun = function(num){
+//        if (num % 2 === 1) {
+//         return "Odd Number";
+//        }
+//        else{
+//         return "Even Number";
+//        }
+//  }
+//  let total = expFun(14);
+//  console.log(total);
+
+// let arr = [1,2,3,4,5,6,7,7,8,9,10];
+// let ans = arr.map((num) => num * 5);
+// console.log(ans); 
+
+// let arr = [1,2,3,4,5,6,7,7,8,9,10];
+// let ans = arr.filter((num)=>num % 2===1);
+// console.log(ans);
+
+// let arr =[1,2,3,4,5,6,7,8,9,10,13];
+
+// let ans = arr.filter((num)=> num % 2==1).map((num)=>num + 1).reduce((value, total)=> value+total ,0);
+// console.log(ans);
+
+// let arr = [1, 2, 3, 4, 5];
+// let ans = arr.filter(filterFn).map(mapFn).reduce(reduceFn, 0);
+// function filterFn(num) {
+//   return num % 2 === 1;
+// }
+// function mapFn(num) {
+//   return num + 1;
+// }
+// function reduceFn(total, value) {
+//   return total + value;
+// }
+// console.log(ans);
+
+// let arr = [1, 2, 3, 4, 5];
+// let ans = arr
+//   .filter((num) => num % 2 === 1)
+//   .map((num) => num + 1)
+//   .reduce((total, value) => total + value, 0);
+// console.log(ans);
+
+// let arr =[1,2,3,4,5,6,7,8,9,10];
+// function Odd(num) {
+//     return num % 2=== 1;
+// }
+// let ans = arr.filter(Odd);
+// console.log(ans);
+
+// function oddMap(num) {
+//     return num * 2;
+// }
+// let result = ans.map(oddMap);
+// console.log(result);
+
+// function sum(value , total) {
+//     return value + total;
+// }
+// let sums = result.reduce(sum ,0);
+// console.log(sums);
+
+// let arr =[1,2,3,4,5,6,7,8,9,10,13];
+
+// let arr = [1, 3, 5, 9, 11 ,5 ,15];
+// let len =0;
+// let ans = arr.every((num)=> num % 2===1);
+// console.log(ans);
+// if (ans) {
+//     len = arr.filter((num)=> num > 5).length;
+// }
+// console.log(len);
+
+// let arr = [1, 3, 5, 9, 5 ,13,15,17,23,21];
+// let len = 0;
+// let ans = arr.every((num) => num % 2 === 1);
+// if (ans) {
+//   len = arr.filter((num) => num > 5).length;
+// }
+// console.log(len);
+
+// function oddEven(num = 10) {
+//     if (num % 2 ===1) {
+//         console.log("odd");
+//     }
+//     else{
+//         console.log("Even");
+//     }
+// }
+// oddEven();
+
+
+// function printAarray() {
+//     for (const value of arguments) {
+//         console.log(value);
+//     }
+// }
+// printAarray(1,2,3,4,5);
+// setTimeout((setime) =>{
+//     console.log("hello ....");
+// },1000);
+
+
+// setTimeout(setime ,1000);
+// setInterval(setime ,1000);
+// function setime() {
+//     console.log("hello ....");
+// }
+
+// function test(num,fun1 , fun2) {
+//     if (num % 2=== 1) {
+//         fun1();
+//     }
+//     else
+//     {
+//         fun2();
+//     }
+// }
+// test(13,function(){
+//      console.log("odd");
+// },function () {
+//     console.log("even");
+    
+// });
+
+// function test(num1 ,num2 , fun1,fun2, fun3 ,fun4) {
+//     if (num1 && num2  > 100) {
+//         fun1();
+//     }
+//     else if (num1 > num2 ) {
+//         fun2();
+//     }
+//     else if ( num2 > num1){
+//         fun3();
+//     }
+//     else if (num1 === num2){
+//      fun4();
+//     }
+// }
+//  test(10 ,20 , () =>{
+//     console.log("fun1");
+//  },() =>{
+//     console.log("fun2");
+//  },() =>{
+//     console.log("fun3");
+//  },() =>{
+//     console.log("fun4");
+//  },)
+
+
+// function oddEven() {
+//     return function (num) {
+//        if (num % 2=== 1) {
+//         return "odd";
+//        }
+//        else{
+//         return "even";
+
+//        }
+//     }
+// }
+// let ans = oddEven();
+//  console.log(ans(10));
+// oddEven()(11);
+
+// const interval = setInterval(() => {
+//   console.log("hello world");
+// }, 1000);
+// setTimeout(function () {
+//   clearInterval(interval);
+// }, 10000);
+
+// const interval = setInterval(()=>{
+//             document.write("hello world");
+// },1000);
+
+// setTimeout (function() {
+//     clearInterval(interval);
+// },20000);
+
+// const Wecode = setInterval(()=>{
+//     console.log("wecode acadmy");
+// },1000)
+
+// setTimeout (function() {
+//     clearInterval(Wecode);
+// },15000);
+
+// function test(){
+//     return function (num) {
+//         if (num % 2=== 1) {
+//             console.log("Number Is Odd");
+//         }
+//         else{
+//             console.log("Number is Even");
+//         }
+//     }
+// }
+// let ans = test();
+// test()(11);
+// ans(10);
+
+// const hello = setInterval(() => {
+//     console.log("hello bilal");
+// }, 1000);
+
+// setTimeout(()=>{
+//       clearTimeout(hello);
+// },15000);
+
+// let obj={
+//     name:"bilal khan",
+//     email:"bilal@gmail.com",
+//     addres:"jaipur",
+//      allDetails:function(){
+//           console.log(`my name is ${this.name} my Email ${this.email} and my addrss ${this.addres }`);
+//      }
+// };
+// obj.allDetails();
+
+// let obj1 ={
+//     name:"bilal",
+//     collageName:"MDS",
+//     address:"jaipur",
+// }
+// let obj2 ={
+//     name:"bilal",
+//     collageName:"MDS",
+//     address:"jaipur",
+   
+// }
+// function allDetails(date,manth , year) {
+//     console.log(`name is ${this.name} collage name ${this.collageName} address name${this.address} biethday ${this.date}`);
+    
+// }
+// allDetails.call(obj1,1995,1,3);
+// obj2.allDetails();
+
+// function test(num){
+//     if (num % 2 === 1) {
+//     return function () {
+//             console.log("Number is Odd");
+//         }
+//     }
+//     if (num % 2 === 0) {
+//     return function() {
+//             console.log("Number is Even");
+//         }
+//     }
+// }
+//  test(17)();
+//  let ans = test(10);
+//  ans();
+
+// let num =0;
+// console.log(++num);
+// console.log(++num);
+// console.log(++num);
+// console.log(++num);
+// console.log(++num);
+// console.log(++num);
+// console.log(++num);
+// console.log(++num);
+// console.log(++num);
+// console.log(++num);
+
+// console.log(++num
+//     ,++num
+//     ,++num
+//     ,++num
+//     ,++num
+//     );
+
+// document.write(++num+"<br>"+ ++num);
+
+// console.log(2 === 2);  // true
+// console.log(2 == "2"); // true
+// console.log(2 === +"2"); // true
+// console.log("learnjavascript" == "learnjavascript"
+// ); // true
+// console.log("LearnJavascript" == "learnjavascript"); //true
+// console.log(0.3 == 0.30); // true
+// console.log(1.0 == 1); // true
+// console.log(1.0 === 1); // true
+
+// Create 5 variables for your bank account details. It would be better if you use let, const and var all 3
+
+// const bankName="HDFC";
+// let accountNumber= 32323434454545;
+// var IFCE = 4343345534;
+// var holderName = "bilal khan";
+// var branchName = "jaipur";
+
+//Write any code and explain that code using multi line comment
+
+// console.log(2 ** 4); 
+//  console.log(14 % 10);
+//  console.log(5 != "5");
+//  console.log(5 !== "5");
+//  console.log(10 <= 5);
+//  console.log(10 >= 10);
+
+// Create a variable marks and assign any number. If marks are greater than 60 then print Pass else print Fail.
+// let marks =61;
+// if (marks >= 60) {
+//     console.log("Pass");
+// }
+// else{
+//     console.log("fail");
+// }
+
+// Take two numbers and check if both numbers are odd or not. If yes then print Odd otherwise print Even
+// let num1 = 10;
+// let num2 = 12;
+
+// if (num1 % 2 ===1 && num2 % 2===1) {
+//     console.log("odd number");
+// }
+// else{
+//     console.log("Even Number");
+// }
+
+
+// let a = 10;
+// let b = 20;
+// let c = 30;
+// let d = 40;
+// a = b;
+// b = c;
+// c = d;
+// d = a;
+// console.log(a , b ,c,d);
+
+// let obj ={
+//     name:"bilal",
+//     address:"jaipur",
+//     email:"bilal@gmail.com",
+//     addharNum:2342353667,
+// }
+
+// let obj2 ={
+//     name:"imran",
+//     address:"merta",
+//     email:"imran@gmail.com",
+//     addharNum:32444555666,
+// }
+// function details(date , mouth ,year) {
+//      console.log(`my is ${this.name} and my address ${this.address} and my email ${this.email} my addhar number ${this.addharNum} my birthday ${date}/${mouth}/${year}`);
+// }
+ 
+// details.call(obj ,21 ,1 ,1995);
+// details.apply(obj ,[21 ,1 ,1995]);
+// details.bind(obj ,21 ,1 ,1995)();
+// details.call(obj2);
+
+// 1 Write a function that uses the call method to print the name of an object.
+// let obj ={
+//     name:"bilal",
+//     address:"jaipur",
+//     email:"bilal@gmail.com",
+//     panNumber:4234234542,
+// }
+// function persnolDetails() {
+//     console.log(`my name ${this.name} ${this.address} ${this.email} ${this.panNumber}`);
+// }
+
+// persnolDetails.call(obj);
+
+// 2 Write a function that uses the apply method to find the minimum value in an array of numbers.
 
 
